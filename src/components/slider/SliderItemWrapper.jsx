901862a9de4c123ebfdb868bf737f008e9a0f813
pilentboy <sirxmahdi79@gmail.com>
2024-04-11 21:27:57 +0430

    change slide class name

--- src/components/slider/SliderItemWrapper.jsx
+++ src/components/slider/SliderItemWrapper.jsx
@@ -1,7 +1,7 @@
 
 export default function SliderItemWrapper({ children }) {
     return (
-        <div className="flex flex-col  items-center space-y-4 test">
+        <div className="flex flex-col  items-center space-y-4 slide-anima">
             {children}
         </div>
     )

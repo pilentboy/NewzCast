47e33e4b6b0c4cdab8510ccad0aca7dac404d54e
pilentboy <sirxmahdi79@gmail.com>
2024-04-13 00:32:08 +0430

    add icons to login authenticate buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Authenticate from './pages/Authenticate';
 import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
 
 	return (
@@ -13,9 +15,10 @@ function App() {
 				<Routes>
 
 					<Route path='/' element={<Landing />} />
-					<Route path='authenticate' element={<Authenticate />}/>
+					<Route path='authenticate' element={<Authenticate />} />
 					<Route path='/login' element={<Login />} />
-					
+					<Route path='/register' element={<Register />} />
+
 				</Routes>
 			</BrowserRouter >
 		</>
--- src/pages/authenticate.jsx
+++ src/pages/authenticate.jsx
@@ -4,7 +4,8 @@ import RegisterLinkItems from "../components/authenticate/RegisterLinkItems"
 import Wrapper from "../components/authenticate/Wrapper"
 import Button from "../components/landing/Button"
 import Slider from "../components/slider/Slider"
-
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebookF } from "react-icons/fa6";
 
 const Authenticate = () => {
 
@@ -22,9 +23,24 @@ const Authenticate = () => {
                     <ColoredLogo />
 
                     <div className="flex flex-col space-y-2  ">
-                        <Button title={'Log In with Email'} styles={'text-purple-1000 text-sm font-bold border border-gray-200  py-4   w-72   '} target={'/login'} />
-                        <Button title={'Log In with Gmail'} styles={'text-gray-500 text-sm font-normal   border border-gray-200  py-4 w-72  hover:text-gray-600 duration-200 '} target={'/'} />
-                        <Button title={'Log In with Facebook'} styles={'text-gray-500 text-sm font-normal   border border-gray-200  py-4 w-72 flex-row-reverse hover:text-gray-600 duration-200 '} target={'/'} />
+                        <Button
+                            title={'Log In with Email'}
+                            styles={'text-purple-1000 text-sm font-bold border border-gray-200  py-4   w-72   '}
+                            target={'/login'} />
+
+                        <Button
+                            title={'Log In with Gmail'}
+                            styles={'text-gray-500 text-sm font-normal   border border-gray-200  py-4 w-72 relative hover:text-gray-600 duration-200 '}
+                            icon={<FcGoogle className="absolute text-lg right-6  traslate-y-[50%]" />}
+                            target={'/'}
+                        />
+
+                        <Button
+                            title={'Log In with Facebook'}
+                            styles={'text-gray-500 text-sm font-normal relative   border border-gray-200  py-4 w-72 flex-row-reverse hover:text-gray-600 duration-200 '}
+                            icon={<FaFacebookF className="absolute  text-lg right-6 text-blue-500  traslate-y-[50%]" />}
+                            target={'/'}
+                        />
                     </div>
 
                     <RegisterLinkItems buttonStyles={'bg-purple-1000 w-80'} />

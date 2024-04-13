ea0f247daafa7f63dbda6f3eefc1ec2cec4302ba
pilentboy <sirxmahdi79@gmail.com>
2024-04-14 01:00:38 +0430

    enable roboto font

--- src/components/authenticate/TermsOfUse.jsx
+++ src/components/authenticate/TermsOfUse.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 export default function TermsOfUse() {
     return <div className="w-56">
-        <span className='text-[12px] text-gray-400 font-medium text-justify  '>
+        <span className='text-[11px] text-gray-400 font-bold text-justify  '>
             I have read and agree to NewzKast’s <Link to={'#'} className='text-purple-1000'>
                 Terms of Use, <Link className='text-purple-1000' target={''}>Privacy Policy</Link>,
             </Link> and <Link to={'#'} className='text-purple-1000'>Disclaimer</Link>.

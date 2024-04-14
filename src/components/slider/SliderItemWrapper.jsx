6270a2a1f4fd721f791f39d91185dc5969332783
pilentboy <sirxmahdi79@gmail.com>
2024-04-14 22:52:32 +0430

    change slider anima scale

--- src/components/slider/SliderItemWrapper.jsx
+++ src/components/slider/SliderItemWrapper.jsx
@@ -1,7 +1,7 @@
 
 export default function SliderItemWrapper({ children }) {
     return (
-        <div className="flex flex-col  items-center space-y-4 slide-anima">
+        <div className="flex flex-col items-center space-y-4 slide-anima">
             {children}
         </div>
     )

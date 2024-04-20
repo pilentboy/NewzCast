3ca0d207fe14f369f25cce25c2ac67ecdbe7313a
pilentboy <sirxmahdi79@gmail.com>
2024-04-20 05:14:50 +0430

    INSTALL FIREBAE

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,9 +1,12 @@
 import Footer from "../components/landing/Footer"
 import Header from "../components/landing/Header"
 import Main from "../components/landing/Main"
-
+import { useEffect } from "react"
+import app from "../firebase-config/config"
 const Landing = () => {
-
+    useEffect(() => {
+        console.log(app, 'firebase')
+    }, [])
     return (
         <>
             <Header />

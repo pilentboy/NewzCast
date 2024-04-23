03ab4b055c3b3e0317872b8ccec43aed0879b65b
pilentboy <sirxmahdi79@gmail.com>
2024-04-23 23:09:35 +0430

    add lg prop to InputWrapper

--- src/components/landing/MainButton.jsx
+++ src/components/landing/MainButton.jsx
@@ -1,8 +1,8 @@
 
-export default function MainButton({ styles, lgBTN, title, type, action }) {
+export default function MainButton({ styles, lgBTN = true, title, type, action }) {
 
     return (
-        <button className={` flex items-center justify-center  rounded-md  text-center  ${lgBTN ? 'w-72' : null} ${styles}`} onClick={() => type !== 'submit' ? action() : null} type={type} >
+        <button className={` flex items-center justify-center  rounded-md  text-center  ${lgBTN ? 'w-72' : 'w-32'} ${styles}`} onClick={() => type !== 'submit' ? action() : null} type={type} >
             {title}
         </button >
     )

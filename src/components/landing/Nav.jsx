df4dd15e2c7a34b218d3f7b969a70ec2fc6147bb
pilentboy <sirxmahdi79@gmail.com>
2024-04-23 23:23:28 +0430

    create NavAuthLinks

--- src/components/landing/Nav.jsx
+++ src/components/landing/Nav.jsx
@@ -1,5 +1,5 @@
 import Logo from "./logo"
-import MainLink from "./MainLink"
+import NavAuthLinks from "./NavAuthLinks"
 
 
 export default function Nav() {
@@ -9,10 +9,7 @@ export default function Nav() {
 
                 <Logo />
 
-                <div className="flex space-x-6">
-                    <MainLink title={'Log in'} target={'authenticate'} styles={'bg-transparent border border-1  border-white  w-28 h-9'} />
-                    <MainLink title={'Sign Up'} target={'/register'} styles={'bg-[#FF9A00] w-28 h-9 '} />
-                </div>
+                <NavAuthLinks />
 
             </div>
         </nav>

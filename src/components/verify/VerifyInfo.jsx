2d4e4f9a85192ace828ebf40f77bb8f3341b142d
pilentboy <sirxmahdi79@gmail.com>
2024-05-01 21:53:42 +0430

    change scroll top anima duration

--- src/components/verify/VerifyInfo.jsx
+++ src/components/verify/VerifyInfo.jsx
@@ -13,12 +13,12 @@ export default function VerifyInfo() {
                 onMouseLeave={() => setIsHovered(false)}
             />
 
-            <div className={`w-[350px] py-2 px-2   rounded-md  -right-10 top-10 bg-white border border-1 border-gray-200  absolute lg:-top-4  lg:right-[-370px] cursor-text ${isHovered ? 'block' : 'hidden'}`}>
+            <div className={`w-[350px] py-2 px-2   rounded-md  -right-10 top-10 bg-white border border-1 border-gray-200 duration-4000 font-sans  absolute lg:-top-4  lg:right-[-370px] cursor-text ${isHovered ? 'block' : 'hidden'}`}>
 
                 <h1 className='text-sm my-1 text-start text-purple-1000 font-medium'>
                     What types of accounts get verified?
                 </h1>
-                <p className='text-gray-400 font-medium text-xs text-justify'>
+                <p className='text-gray-400 font-medium text-xs text-justify '>
                     An account may be verified if it is determined to be an
                     account of public interest. Typically this includes accounts
                     maintained by users in media, journalism, government,

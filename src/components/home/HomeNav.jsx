e96ce191523354e91e16869131708cf3d47e2f7f
pilentboy <sirxmahdi79@gmail.com>
2024-05-01 22:51:49 +0430

    add outlet to homenav

--- src/components/home/HomeNav.jsx
+++ src/components/home/HomeNav.jsx
@@ -1,10 +1,16 @@
 import ColoredLogo from "../ColoredLogo"
+import { Outlet } from "react-router-dom"
 
 function HomeNav() {
     return (
-        <div className="w-lvw relative px-4 py-10 bg-white ">
-            <ColoredLogo styles={'absolute left-10 top-2'} size={'h-16'} />
-        </div>
+        <>
+            <div className="w-lvw relative px-4 py-10 bg-white ">
+                <ColoredLogo styles={'absolute left-10 top-2'} size={'h-16'} />
+
+            </div>
+            <Outlet />
+        </>
+
     )
 }
 

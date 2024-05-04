0ba459d001980c21f6ca2b931060df7f24974c59
pilentboy <sirxmahdi79@gmail.com>
2024-05-04 23:36:24 +0430

    fix ladning Wrapper height

--- src/components/authenticate/Wrapper.jsx
+++ src/components/authenticate/Wrapper.jsx
@@ -1,7 +1,7 @@
 
 export default function Wrapper({ styles, children }) {
     return (
-        <div className={`w-screen min-h-lvh py-2 relative flex items-center justify-around  ${styles}`}>
+        <div className={`w-screen min-h-screen py-2 relative flex items-center justify-around  ${styles}`}>
             {children}
         </div>
     )

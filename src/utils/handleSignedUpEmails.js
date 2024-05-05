746a953b4957a3f108f8506b15b1e679b0e974fb
pilentboy <sirxmahdi79@gmail.com>
2024-05-05 21:43:38 +0430

    fix supabseadmin

--- src/utils/handleSignedUpEmails.js
+++ src/utils/handleSignedUpEmails.js
@@ -1,4 +1,4 @@
-import supabaseAdmin from "./supabaseAdmin"
+import supabaseAdmin from "./SupabaseAdmin"
 
 async function HandleSignedUpEmails(userEmail) {
     const { data: { users } } = await supabaseAdmin.auth.admin.listUsers()

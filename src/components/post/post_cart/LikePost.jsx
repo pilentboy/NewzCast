a88f68bbc16b3ac981da65ed7358dfec063991c9
pilentboy <sirxmahdi79@gmail.com>
2024-05-06 22:29:47 +0430

    fix LikePost text issue

--- src/components/post/post_cart/LikePost.jsx
+++ src/components/post/post_cart/LikePost.jsx
@@ -6,13 +6,13 @@ function LikePost({ likedPost, handleLike }) {
 
     return (
         <button type='button' className='flex items-center text-gray-600 text-[12px] font-medium'
-            onClick={() => handleLike(e => !e) }
+            onClick={() => handleLike(e => !e)}
         >
 
             <BiSolidLike
                 className={`text-2xl mr-2 duration-200 ${!likedPost ? 'text-gray-400' : 'text-green-600 '} `}
             />
-            <span className={`${!likedPost ? 'invisible' : 'visible'}`}>
+            <span className={`${likedPost ? 'invisible' : 'visible'}`}>
                 Like
             </span>
         </button>)

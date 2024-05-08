1e83d34017df10a2a9d1154c76f49474a69f8413
pilentboy <sirxmahdi79@gmail.com>
2024-05-09 01:06:32 +0430

    handle addToFavorite if user is not loged in

--- src/components/post/post_cart/AddFavorites.jsx
+++ src/components/post/post_cart/AddFavorites.jsx
@@ -1,11 +1,18 @@
 import { FaHeart } from "react-icons/fa";
-
+import { useContext } from "react";
+import { LoginContext } from "../../../context/LoginContext";
 function AddFavorites({ favoritePost, handleFavorite }) {
+
+    const { userInfo } = useContext(LoginContext)
+
+    const handleAddFavorite = () => {
+        userInfo ? handleFavorite(e => !e) : alert("Please log in first!")
+    }
     return (
-        <button type='button' onClick={() => handleFavorite(e => !e)}>
+        <button type='button' onClick={handleAddFavorite}>
             <FaHeart
                 title='add to favorites'
-                className={` text-base duration-200  ${favoritePost ? 'text-red-700' : 'text-gray-400'}`} />
+                className={` text-base duration-200  ${userInfo && favoritePost ? 'text-red-700' : 'text-gray-400'}`} />
 
         </button>)
 }

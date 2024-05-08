53ec3352e007c6032e53dcf0317806dcb07e1502
pilentboy <sirxmahdi79@gmail.com>
2024-05-09 01:02:09 +0430

    handle like if user is not loged in

--- src/components/post/post_cart/LikePost.jsx
+++ src/components/post/post_cart/LikePost.jsx
@@ -1,18 +1,24 @@
 import { BiSolidLike } from "react-icons/bi";
+import { useContext } from "react";
+import { LoginContext } from "../../../context/LoginContext";
 
 function LikePost({ likedPost, handleLike }) {
 
+    const { userInfo } = useContext(LoginContext)
 
+    const handlePostLike = () => {
+        userInfo ? handleLike(e => !e) : alert("Please log in first!")
+    }
 
     return (
         <button type='button' className='flex items-center text-gray-600 text-[12px] font-medium'
-            onClick={() => handleLike(e => !e)}
+            onClick={handlePostLike}
         >
 
             <BiSolidLike
-                className={`text-2xl mr-2 duration-200 ${!likedPost ? 'text-gray-400' : 'text-green-600 '} `}
+                className={`text-2xl mr-2 duration-200 ${!likedPost || !userInfo ? 'text-gray-400' : 'text-green-600 '} `}
             />
-            <span className={`${likedPost ? 'invisible' : 'visible'}`}>
+            <span className={`${likedPost && userInfo ? 'invisible' : 'visible'}`}>
                 Like
             </span>
         </button>)
--- src/components/post/post_cart/PostInfoContainer.jsx
+++ src/components/post/post_cart/PostInfoContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 function PostInfoContainer({ children,styles }) {
     return (

62a46a6c379c83ec6c71e6d5d751425d6a85fbfb
pilentboy <sirxmahdi79@gmail.com>
2024-05-13 21:01:35 +0430

    add gray-500 to newzcast nav links

--- src/components/home/HomeNavLink.jsx
+++ src/components/home/HomeNavLink.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 function HomeNavLink({ styles, children, target, linkTitle, button }) {
     return (
         <li className={`flex items-center text-gray-400 ${styles}`}>
-            <Link to={target} className="text-gray-400 flex items-center font-sans text-sm hover:text-gray-600 duration-200" >
+            <Link to={target} className="text-gray-500 flex items-center font-sans text-sm hover:text-gray-600 duration-200" >
                 <div className="mx-2">
                     {children}
                 </div>

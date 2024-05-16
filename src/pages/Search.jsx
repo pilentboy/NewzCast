82dda6ca29124b5569f888285525363fc3a4a99d
pilentboy <sirxmahdi79@gmail.com>
2024-05-17 00:16:40 +0430

    test searching

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react"
 import HomeContainer from "../components/home/HomeContainer"
+import userdata from '../db.json'
 
 function Search() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const test = userdata['UsersData'].filter((username) => username.name.toLowerCase().includes('a'))
+    setUsers(test)
+  }, [])
   return (
     <HomeContainer>
       <h1>search</h1>
+      <h1>result</h1>
+      <ul>
+        {
+          users.map((user, id) => (
+            <li key={id}>
+              {user.username}
+            </li>
+          )
+          )}
+      </ul>
     </HomeContainer>
   )
 }

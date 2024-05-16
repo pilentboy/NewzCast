98cabfd286c0ed6085077396c9e0540dc20ea9a8
pilentboy <sirxmahdi79@gmail.com>
2024-05-17 02:38:00 +0430

    display user's full name in Profile page

--- src/components/home/NavTopSmallScreen.jsx
+++ src/components/home/NavTopSmallScreen.jsx
@@ -1,12 +1,18 @@
-import UserPic from '../../assets/images/userprof.jpg'
 import ColoredLogo from '../ColoredLogo'
 import UserProfile from '../profile/UserProfile'
+import { useContext } from 'react'
+import { LoginContext } from '../../context/LoginContext'
 function NavTopSmallScreen() {
+
+    const { userDBJsonInfo } = useContext(LoginContext)
+    console.log(userDBJsonInfo, "tqtq")
     return (
         <div className="flex justify-between items-center w-full px-5 py-2 border-b-8 border-gray-200  bg-white md:hidden ">
             <span> </span>
             <ColoredLogo styles={'ms-6'} />
-            <UserProfile username={'Mahdi'} styles={'flex-col mt-3'} profileImage={UserPic} imageSize={'h-8'} />
+            {
+                userDBJsonInfo && <UserProfile userFullName={userDBJsonInfo['firstName']} styles={'flex-col mt-3'} profileImage={userDBJsonInfo['profileImage']} imageSize={'h-8'} />
+            }
         </div>)
 
 }

9c4e5f23e18cb371e3d4cfb2d6a9a7655f0f2303
pilentboy <sirxmahdi79@gmail.com>
2024-05-28 05:20:54 +0430

    create Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import HomeContainer from "../components/home/HomeContainer"
+
+
+const Home = () => {
+
+    return (
+        <HomeContainer>
+            <h1>Home</h1>
+        </HomeContainer>
+    )
+}
+
+export default Home

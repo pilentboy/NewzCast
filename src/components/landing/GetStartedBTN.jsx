2c8b7e96b8332f1ac129d97bb17b09d459da9a3f
pilentboy <sirxmahdi79@gmail.com>
2024-05-31 06:12:06 +0430

    display getStarted btn in header small screen

--- src/components/landing/GetStartedBTN.jsx
+++ src/components/landing/GetStartedBTN.jsx
@@ -1,8 +1,8 @@
 import MainLink from "./MainLink"
 
-export default function GetStartedBTN({ anima = false }) {
+export default function GetStartedBTN({ anima = false ,styles}) {
     return (
-        <MainLink title={'Get Started'} target={'/newzcast'} styles={` font-bold bg-purple-1000 duration-150  w-28 h-9 text-sm ${anima ? 'hover:ms-2 ' : null}`} />
+        <MainLink title={'Get Started'} target={'/newzcast'} styles={` font-bold bg-purple-1000 duration-150  w-28 h-9 text-sm ${anima ? 'hover:ms-2 ' : null} ${styles}`} />
 
     )
 }
--- src/components/landing/Header.jsx
+++ src/components/landing/Header.jsx
@@ -2,17 +2,33 @@ import Nav from "./Nav"
 import Container from "./Container"
 import appScreenshot from '../../assets/images/homephoneview.png'
 import GetStartedBTN from "./GetStartedBTN";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppDownloadLinks from "./AppDownloadLinks";
 import { FaCircleArrowUp } from "react-icons/fa6";
 
 export default function LandingHeader() {
 
     const [scrollyValue, setScrollYValue] = useState(window.scrollY)
+    const [headerAnima,setHeaderAnime]=useState()
 
     document.addEventListener('scroll', () => {
         setScrollYValue(window.scrollY)
     })
+
+    window.addEventListener("resize",()=>{
+        window.innerWidth < 640  ? setHeaderAnime('screen-shot-anima') : setHeaderAnime(null)
+    })    
+
+
+    useEffect(()=>{
+        
+        setTimeout(()=>{
+            window.innerWidth < 640  ? setHeaderAnime('screen-shot-anima') : setHeaderAnime(null)
+        },600)
+
+    },[])
+
+    
 
     return (
         <header id="header" className="header-bg h-full">
@@ -21,7 +37,7 @@ export default function LandingHeader() {
 
                 <Nav />
 
-                <Container styles={'mt-28 md:mt-14'} noContainer={true}>
+                <Container styles={'mt-28 md:mt-14  '} noContainer={true}>
                     <div className="flex flex-col items-center  lg:items-start  ">
                         <div className="space-y-7 my-16 hidden md:block lg:my-0">
                             <h1 className="text-white text-6xl  font-bold"> Share the Newz!</h1>
@@ -29,7 +45,17 @@ export default function LandingHeader() {
                         </div>
                         <AppDownloadLinks />
                     </div>
-                    <img src={appScreenshot} alt="app screenshot" className="lg:w-[250px] lg:h-[460px] duration-150 lg:hover:scale-110 " />
+                    <div className="flex items-center w-full justify-center md:w-fit ">
+
+                    <img src={appScreenshot} alt="app screenshot" 
+                    className={`lg:w-[250px]  lg:h-[460px] duration-150 lg:hover:scale-110 
+                    ${headerAnima}
+                    `} />
+
+                    <GetStartedBTN anima={true} styles={`${headerAnima === 'screen-shot-anima' ? 'started-btn-anima' : 'hidden'}  `} />
+
+                    </div>
+                
                 </Container>
 
                 {

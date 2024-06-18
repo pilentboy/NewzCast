d6b508253af234751379721fe625b29f889c21bd
pilentboy <sirxmahdi79@gmail.com>
2024-06-18 06:00:40 +0430

    add icon to CommentBoxModal top

--- src/components/modal/CommentBoxModal.jsx
+++ src/components/modal/CommentBoxModal.jsx
@@ -5,7 +5,7 @@ function CommentBoxModal({children}) {
   return (
     <div className="absolute bottom-0 left-[50%] translate-x-[-50%] mx-auto bg- flex flex-col w-screen overflow-y-auto h-[500px] bg-black border-white  border-t-2 rounded-t-lg px-3 bottom-box-anima lg:h-[400px] lg:w-3/6">
       <CgBorderStyleSolid className="text-white mx-auto mt-2"/>
-      <h1  className="text-center text-stone-600  text-base mt-1  ">comments</h1>
+      <h1  className="text-center text-stone-600  text-sm mt-1  ">comments</h1>
       {children}
     </div>
   )

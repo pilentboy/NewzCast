dec0f4607cb3e56fc690e2d279e46bfb3ffd5ea6
pilentboy <sirxmahdi79@gmail.com>
2024-06-18 05:59:48 +0430

    add icon to CommentBoxModal top

--- src/components/modal/CommentBoxModal.jsx
+++ src/components/modal/CommentBoxModal.jsx
@@ -1,9 +1,11 @@
 
+import { CgBorderStyleSolid } from "react-icons/cg";
 
 function CommentBoxModal({children}) {
   return (
     <div className="absolute bottom-0 left-[50%] translate-x-[-50%] mx-auto bg- flex flex-col w-screen overflow-y-auto h-[500px] bg-black border-white  border-t-2 rounded-t-lg px-3 bottom-box-anima lg:h-[400px] lg:w-3/6">
-      <h1  className="text-center text-white font-bold text-2xl mt-1  ">comments</h1>
+      <CgBorderStyleSolid className="text-white mx-auto mt-2"/>
+      <h1  className="text-center text-stone-600  text-base mt-1  ">comments</h1>
       {children}
     </div>
   )

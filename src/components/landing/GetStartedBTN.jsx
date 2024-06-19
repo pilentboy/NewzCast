2c57df3f7fead1f316702b82e26dc8468d40bdc3
pilentboy <sirxmahdi79@gmail.com>
2024-06-19 11:52:47 +0430

    use animate_flash in getStarted btn

--- src/components/landing/GetStartedBTN.jsx
+++ src/components/landing/GetStartedBTN.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function GetStartedBTN({styles,hover}) {
     return (
-		 <Link to={'newzcast'} className={`w-28 py-2  bg-purple-1000  rounded-md  text-center ${hover? 'hover:ms-6' : null} duration-500 ${styles}`}>
+		 <Link to={'newzcast'} className={`w-28 py-2  bg-purple-1000  rounded-md animate__animated animate__flash text-center ${hover? 'hover:ms-6' : null} duration-500 ${styles}`}>
 			Get Started
         </Link >
     )

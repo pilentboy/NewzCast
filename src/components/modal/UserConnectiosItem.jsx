3bf9c4267b113fd78a56ff8c0bf4c427c765e311
pilentboy <sirxmahdi79@gmail.com>
2024-06-21 13:54:20 +0430

    handle display users connections

--- src/components/modal/UserConnectiosItem.jsx
+++ src/components/modal/UserConnectiosItem.jsx
@@ -2,14 +2,14 @@ import UserProfile from "../profile/UserProfile"
 import MainButton from "../landing/MainButton"
 import { useState } from "react"
 
-function UserConnectiosItem({ name, profileImage, followState }) {
+function UserConnectiosItem({ name, profileImage,email, followState}) {
 
     const [connectionState,setConnectionState]=useState(followState ? followState : false)
 
     
     return (
         <div className='border border-gray-200 rounded-md flex items-center justify-between my-1 p-2'>
-            <UserProfile styles='space-x-2' name={name} profileImage={profileImage} />
+            <UserProfile styles='space-x-2' name={name}  target={`/newzcast/profile/${email}`} profileImage={profileImage}  />
             <MainButton
                 title={connectionState ? 'Following' : 'Follow'}
                 styles={`w-[100px] h-8  text-sm font-normal  ${connectionState ? ' bg-transparent border border-purple-1000 text-purple-1000' : 'bg-purple-1000 text-white'}`}
--- src/components/modal/UserConnectiosList.jsx
+++ src/components/modal/UserConnectiosList.jsx
@@ -1,27 +1,51 @@
-import test from '../../assets/images/1.webp'
+import { useContext, useEffect, useState } from 'react'
 import UserConnectiosItem from './UserConnectiosItem'
+import { LoginContext } from '../../context/LoginContext'
 
 function UserConnectiosList({ title,userEmail}) {
 
-    
+    const {mainDB}=useContext(LoginContext)
+    const [connectionUsersname,setConnectionUsernames]=useState([])
+
+    const handleConnectionList=()=>{
+        const user=mainDB.filter(user => user.email === userEmail)[0]
+        const connectionList=user['userConnections'].filter(connectionType => connectionType[title])[0]
+        const connectionUsernames= connectionList[Object.keys(connectionList)[0]]
+        return connectionUsernames
+    }
+
+    useEffect(()=>{
+        if(title){
+            setConnectionUsernames(handleConnectionList())
+        }
+    },[title])
 
 
     return (
-        <div className="w-[330px] h-[400px] p-4  bg-white rounded-md flex flex-col">
+        <div className="w-[330px] h-[400px] p-4  bg-white rounded-md flex flex-col relative">
             <div className="flex justify-center items-center pb-4">
                 <span className=' font-bold text-[15px]'>{title}</span>
             </div>
 
             <div className='overflow-y-auto  no-scrollbar'>
 
-                <UserConnectiosItem name={'pilentboy'} profileImage={test} followState={true} />
-                <UserConnectiosItem name={'pilentboy'} profileImage={test} followState={false} />
-                <UserConnectiosItem name={'pilentboy'} profileImage={test} followState={false} />
-                <UserConnectiosItem name={'pilentboy'} profileImage={test} followState={true} />
-                <UserConnectiosItem name={'pilentboy'} profileImage={test} followState={false} />
-                <UserConnectiosItem name={'pilentboy'} profileImage={test} followState={true} />
-                <UserConnectiosItem name={'pilentboy'} profileImage={test} followState={false} />
-                <UserConnectiosItem name={'pilentboy'} profileImage={test} followState={true} />
+                {
+                    connectionUsersname.length > 0 ? (
+                        mainDB.map((user,index) => {
+                            return (
+                                connectionUsersname.map(username => {
+                                    if(user.username === username){
+                                        return  <UserConnectiosItem name={username} profileImage={user.profileImage} email={user.email} followState={true} 
+                                        key={index} />
+                                    }
+                                   
+                                })
+                            )
+                     
+                        })
+                    ) : <h1 className='absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]'>No {title}</h1>
+                }
+             
             </div>
 
         </div>

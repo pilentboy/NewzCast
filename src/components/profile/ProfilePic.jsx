19fcffe74187b33844ade4ce1154be237a34b0d9
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-08 01:34:14 +0330

    fix displaying profile img

--- src/components/profile/ProfilePic.jsx
+++ src/components/profile/ProfilePic.jsx
@@ -3,7 +3,7 @@ import blank_img from '../../assets/images/blank_profile.webp'
 function ProfilePic({ size, src, imgStyles, button }) {
     return (
         <div className={button ? 'relative' : null} >
-            <img src={src ? src : blank_img} className={`rounded-full ${!size ? 'w-7 h-7' : size}  ${imgStyles}  `} alt="user profile image" />
+            <img src={src ? src : blank_img} className={`rounded-full object-cover	 ${!size ? 'w-7 h-7' : size}  ${imgStyles}  `} alt="user profile image" />
 
             {button}
 

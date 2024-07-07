58f44adec29677fc443e2144e263823585e885bc
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-08 01:06:58 +0330

    create smallAlertBox

---
+++ src/components/alert/smallAlertBox.jsx
@@ -0,0 +1,11 @@
+
+const SmallAlertBox=({bgColor})=>{
+
+    return (
+        <div className={`w-24 h-4  ${bgColor} `}>
+            <span className="text-white font-medium text-base">
+                I've just installed vs code!
+            </span>
+        </div>
+    )
+}

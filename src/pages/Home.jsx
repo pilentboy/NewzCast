a9bf86842f43485981ca70797ca37810d22e3dc4
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-09 00:18:45 +0330

    display login message in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import HomeContainer from "../components/home/HomeContainer"
-import {useEffect} from 'react'
+import {useEffect, useContext} from 'react'
+import { Link } from "react-router-dom"
+import { LoginContext } from "../context/LoginContext"
 
 const Home = () => {	
+   
+    const {userLoggedInfo} = useContext(LoginContext)
 
 	useEffect(()=>{
 		document.title = "Home"
@@ -9,7 +13,9 @@ const Home = () => {
 
     return (
         <HomeContainer>
-            <h1>Home</h1>
+            {
+                userLoggedInfo ? <span>posts</span> : <span className="border-b border-gray-400 ">Please <Link to={'/authenticate'} className="font-bold text-black">Log in</Link> to see your followers news</span>
+            }
         </HomeContainer>
     )
 }

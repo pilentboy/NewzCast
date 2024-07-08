ba29a842a41243d86a078cde7d854145de5a4a2f
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-09 00:46:07 +0330

    display user followers posts in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,44 @@
 import HomeContainer from "../components/home/HomeContainer"
-import {useEffect, useContext} from 'react'
+import {useEffect, useContext, useState} from 'react'
 import { Link } from "react-router-dom"
 import { LoginContext } from "../context/LoginContext"
+import PostWrapper from "../components/post/post_cart/PostWrapper"
 
 const Home = () => {	
-   
-    const {userLoggedInfo} = useContext(LoginContext)
+    
+    const [userFollowers,setUserFollowers]=useState(null)
+    const {userLoggedInfo, mainDB} = useContext(LoginContext)
+
 
 	useEffect(()=>{
+        if(userLoggedInfo){
+            const followers=userLoggedInfo['userConnections'][0]['Followers']
+            if(followers.length > 0){
+                setUserFollowers(followers)
+            }else{
+                setUserFollowers(null)
+            }
+        }
 		document.title = "Home"
-	},[])
+	},[userLoggedInfo])
 
     return (
         <HomeContainer>
             {
-                userLoggedInfo ? <span>posts</span> : <span className="border-b border-gray-400 ">Please <Link to={'/authenticate'} className="font-bold text-black">Log in</Link> to see your followers news</span>
+                userLoggedInfo && userFollowers ? (
+                    userFollowers.map(follower => (
+                        mainDB.map(user => (
+                            user.username === follower ? (
+                                user.posts.map(post => (
+                                    <PostWrapper key={post.postID} userPostsInfo={post} userEmail={user.email} userName={user.username} profileImg={user.profileImage} />
+                                ))
+                            ) : null
+                        ))
+                    )
+                    )
+                )
+
+                : <span className="border-b border-gray-400 ">Please <Link to={'/authenticate'} className="font-bold text-black">Log in</Link> to see your followers news</span>
             }
         </HomeContainer>
     )

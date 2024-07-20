934c8128bffdf9b9425a6262e572b8436ea1b433
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-21 02:51:29 +0330

    remove mainBTN from UserConnectiosItem

--- src/components/modal/UserConnectiosItem.jsx
+++ src/components/modal/UserConnectiosItem.jsx
@@ -1,13 +1,8 @@
 import UserProfile from "../profile/UserProfile"
-import MainButton from "../landing/MainButton"
-import { useState } from "react"
 import ConnectionBTN from "../profile/ConnectionBTN"
 
 function UserConnectiosItem({ name, profileImage,email, followState}) {
 
-    const [connectionState,setConnectionState]=useState(followState ? followState : false)
-
-    
     return (
         <div className='border border-gray-200  rounded-md flex items-center justify-between my-1 p-2'>
             <UserProfile styles='space-x-2' name={name}  target={`/newzcast/profile/${email}`} profileImage={profileImage}  />

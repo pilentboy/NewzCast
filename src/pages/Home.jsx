9c449c32c89c70c9bc7b5b2662c8d213ed2f7f8f
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-21 15:03:58 +0330

    fix displaying following in Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,31 +3,40 @@ import {useEffect, useContext, useState} from 'react'
 import { Link } from "react-router-dom"
 import { LoginContext } from "../context/LoginContext"
 import PostWrapper from "../components/post/post_cart/PostWrapper"
+import Loading from "../components/Loading"
 
 const Home = () => {	
     
     const [userFollowing,setUserFollowing]=useState(null)
     const {userLoggedInfo, mainDB} = useContext(LoginContext)
-
+    const [loading,setLoading]=useState(true)
 
 	useEffect(()=>{
+        setLoading(true)
         if(userLoggedInfo){
             const following=userLoggedInfo['userConnections'][1]['Following']
             if(following.length > 0){
                 setUserFollowing(following)
             }else{
-                setUserFollowing(null)
+                setUserFollowing(false)
             }
         }
+        setLoading(false)
 		document.title = "Home"
 	},[userLoggedInfo])
 
-    return (
-        <HomeContainer>
+
+
+    if(loading) return <Loading loading={loading} />
+    if(!userLoggedInfo) {
+        return <HomeContainer>
+                    <span className="border-b border-gray-400 mt-10">Please <Link to={'/authenticate'} className="font-bold text-black">Log in</Link> to see your following news</span>
+            </HomeContainer>
+    }
+    if(userFollowing){
+        return <HomeContainer>
             {
-                
-                userLoggedInfo && userFollowing ? (
-                    userFollowing.map(following => (
+                userFollowing.map(following => (
                         mainDB.map(user => (
                             user.username === following ? (
                                 user.posts.map(post => (
@@ -35,14 +44,17 @@ const Home = () => {
                                 ))
                             ) : null
                         ))
-                    )
-                    )
-                )
-
-                : <span className="border-b border-gray-400 mt-10">Please <Link to={'/authenticate'} className="font-bold text-black">Log in</Link> to see your following news</span>
+                    ) )
             }
         </HomeContainer>
-    )
+        
+    }else{
+        return  <HomeContainer>
+              <span className="border-b border-gray-400 mt-10">You Have No Following</span>
+                </HomeContainer>
+            }
+
+
 }
 
 export default Home

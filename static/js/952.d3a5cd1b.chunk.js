4821b67069bce0e52ab0864c9a2293ad47d8dee4
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-08-02 21:52:21 +0330

    Updates

---
+++ static/js/952.d3a5cd1b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknewzCast=self.webpackChunknewzCast||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.d3a5cd1b.chunk.js.map
